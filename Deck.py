ac6513a3f86ecdd6c560affaa678e3c22e924390
Jalen <jalen.gill@macaulay.cuny.edu>
2025-09-02 09:47:08 -0400

    Edited deck logic back to normal

--- Deck.py
+++ Deck.py
@@ -3,8 +3,8 @@ import random
 class Deck:
     def __init__(self, deck_count=1):
         self.card_values = {
-            # "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10,
-            # "J": 10, "Q": 10, "K": 10, "A": 11,
+            "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10,
+            "J": 10, "Q": 10, "K": 10, "A": 11,
             "10": 10, "A": 11
         }
         self.cards = self.create_deck(deck_count)
@@ -13,7 +13,7 @@ class Deck:
     def create_deck(self, deck_count):
         deck = []
         for card, value in self.card_values.items():
-            for _ in range(deck_count * 26):  # Each card appears 4 times per deck
+            for _ in range(deck_count * 4):  # Each card appears 4 times per deck
                 deck.append(card)
         return deck
 
